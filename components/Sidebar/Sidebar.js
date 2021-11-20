bd5699bc473b426ca1ec4f9a2c76f6118bcef490
p0kem0nEmerald <intheflask@gmail.com>
2021-11-20 15:40:40 +0900

    :art: Minor Updates for design.

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -4,7 +4,7 @@
  * @copyright エメラルドを風化させないChannel 2021
  * @license MIT
  */
- 
+
 import * as React from "react";
 
 import { Contents } from "data/js/contents";
@@ -21,7 +21,7 @@ export default function Sidebar() {
   const router = useRouter();
   return (
     <>
-      <nav className="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden md:bg-blueGray-800 shadow-xl bg-white flex flex-wrap items-center justify-between relative md:w-64 z-10 py-4 px-6">
+      <nav className="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden md:bg-white shadow-xl bg-blueGray-800 flex flex-wrap items-center justify-between relative md:w-64 z-10 py-4 px-6">
         <div className="px-0 flex flex-wrap items-center justify-between w-full mx-auto md:flex-col md:items-stretch md:min-h-full md:flex-nowrap">
           {/* Toggler */}
           <button
@@ -59,10 +59,7 @@ export default function Sidebar() {
               <div className="flex flex-wrap">
                 <div className="w-6/12">
                   <Link href="/">
-                    <a
-                      href="#pablo"
-                      className="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm font-bold p-4 px-0"
-                    >
+                    <a className="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm font-bold p-4 px-0">
                       LeoTube
                     </a>
                   </Link>
